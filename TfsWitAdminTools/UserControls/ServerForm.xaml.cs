ed27afc21cfe37033e3276d0d1bf47dfd9860481
mahdiehsanifar <mahdi.ehsanifar@gmail.com>
2015-09-27 20:06:01 +0330

    Fix first and second tools issues

--- TfsWitAdminTools/UserControls/ServerForm.xaml.cs
+++ TfsWitAdminTools/UserControls/ServerForm.xaml.cs
@@ -16,27 +16,27 @@ namespace TfsWitAdminTools.UserControls
                 txtCategories.Syntax = TextEditorEx.SyntaxNames.XML;
         }
 
-        private void expFirstServerWITypes_Expanded(object sender, RoutedEventArgs e)
+        private void expWITypes_Expanded(object sender, RoutedEventArgs e)
         {
-            grwFirstServerWITypes.Height = new GridLength(1, GridUnitType.Star);
+            grwWITypes.Height = new GridLength(1, GridUnitType.Star);
             e.Handled = true;
         }
 
-        private void expFirstServerWITypes_Collapsed(object sender, RoutedEventArgs e)
+        private void expWITypes_Collapsed(object sender, RoutedEventArgs e)
         {
-            grwFirstServerWITypes.Height = new GridLength(1, GridUnitType.Auto);
+            grwWITypes.Height = new GridLength(1, GridUnitType.Auto);
             e.Handled = true;
         }
 
-        private void expFirstServer_Expanded(object sender, RoutedEventArgs e)
+        private void expServer_Expanded(object sender, RoutedEventArgs e)
         {
-            grwFirstServerMain.Height = new GridLength(4, GridUnitType.Star);
+            grwServer.Height = new GridLength(4, GridUnitType.Star);
             e.Handled = true;
         }
 
-        private void expFirstServer_Collapsed(object sender, RoutedEventArgs e)
+        private void expServer_Collapsed(object sender, RoutedEventArgs e)
         {
-            grwFirstServerMain.Height = new GridLength(1, GridUnitType.Auto);
+            grwServer.Height = new GridLength(1, GridUnitType.Auto);
             e.Handled = true;
         }
 
